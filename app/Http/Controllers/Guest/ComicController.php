c88658c624d1a78ab90aa63d28f406aaade51b07
Valerio Salzano <vlr.slzn@gmail.com>
2023-05-17 15:19:56 +0200

    added delete method

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -97,8 +97,10 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Comic $comic)
     {
-        //
+        $comic ->delete();
+
+        return redirect()->route('comics.index');
     }
 }
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,31 +1,46 @@
 @extends('layouts.app')
 
-@section('page-title', $comic->title );
+@section('page-title', $comic->title);
 
 @section('content')
 
-<div class="container py-5">
+    <div class="container py-5">
 
-    <div class="card text-bg-dark mb-3">
-        <div class="row g-0">
-            <div class="col-md-4 overflow-hidden">
-                <img src="{{$comic->thumb}}" class="rounded-start object-fit-cover h-100" alt="{{$comic->title}}">
-            </div>
-            <div class="col-md-8 py-3">
-                <div class="card-body">
-                    <h5 class="card-title fs-3 border-bottom">{{$comic->title}}</h5>
-                    <h6 class="card-title fs-5">{{$comic->series}}</h6>
-                    <p class="card-text">{{$comic->description}}</p>
-                    <p class="card-text">Price: {{$comic->price}}</p>
-                    <p class="card-text">Date: {{$comic->sale_date}}</p>
+        <div class="card text-bg-dark mb-3">
+            <div class="row g-0">
+                <div class="col-md-4 overflow-hidden">
+                    <img src="{{ $comic->thumb }}" class="rounded-start object-fit-cover h-100" alt="{{ $comic->title }}">
+                </div>
+                <div class="col-md-8 py-3">
+                    <div class="card-body">
+                        <h5 class="card-title fs-3 border-bottom">{{ $comic->title }}</h5>
+                        <h6 class="card-title fs-5">{{ $comic->series }}</h6>
+                        <p class="card-text">{{ $comic->description }}</p>
+                        <p class="card-text">Price: {{ $comic->price }}</p>
+                        <p class="card-text">Date: {{ $comic->sale_date }}</p>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div class="d-flex row justify-content-between">
+            <div class="col">
+                <a href="{{ route('comics.index') }}" class="btn btn-primary w-100">Return to comics list</a>
+            </div>
+            <div class="col">
+                <a class="w-100 btn btn-warning" href="{{ route('comics.edit', ['comic' => $comic->id]) }}"> Edit comic </a>
+            </div>
+            <div class="col">
+                <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="post" class="col">
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" class="btn btn-danger w-100"> Delete Comic </button>
+                </form>
+            </div>
+        </div>
+
     </div>
 
-    <a href="{{ route('comics.index') }}" class="btn btn-primary">Return to comics list</a>
-    <a class="btn btn-warning" href="{{route('comics.edit', ['comic' => $comic->id])}}"> Edit comic </a>
-</div>
 
-    
 @endsection
